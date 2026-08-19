6cc8e9590c11bddb3b75c23410150c58c0921582
nassimduclos27-max <nassim.duclos27@gmil.com>
2026-08-19 15:19:36 +0200

    Add API client

---
+++ src/api/client.js
@@ -0,0 +1,15 @@
+const API_URL = "https://predictiveflow-production.up.railway.app";
+
+export const api = async (endpoint, options = {}) => {
+  const token = localStorage.getItem("token");
+  const res = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...options.headers
+    }
+  });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+};
